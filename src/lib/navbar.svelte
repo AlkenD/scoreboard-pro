86ef05549e55bc2fb0516ccd6965883f7db6d061
AlkenD <desteralken@gmail.com>
2024-04-15 02:20:51 +0530

    feat: new layout

--- src/lib/navbar.svelte
+++ src/lib/navbar.svelte
@@ -1,6 +1,21 @@
-<a
-	href="/"
-	class="text-center p-2 font-bold uppercase sticky top-0 bg-white/20 backdrop-blur-lg block"
->
-	St. Francis College
-</a>
+<div class="sticky top-0 bg-blue-700/80 backdrop-blur-lg">
+	<div class="w-full flex justify-center items-center relative p-2">
+		<a href="/" class="text-center p-2 font-bold uppercase absolute left-2">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="icon icon-tabler icons-tabler-filled icon-tabler-home"
+				><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path
+					d="M12.707 2.293l9 9c.63 .63 .184 1.707 -.707 1.707h-1v6a3 3 0 0 1 -3 3h-1v-7a3 3 0 0 0 -2.824 -2.995l-.176 -.005h-2a3 3 0 0 0 -3 3v7h-1a3 3 0 0 1 -3 -3v-6h-1c-.89 0 -1.337 -1.077 -.707 -1.707l9 -9a1 1 0 0 1 1.414 0m.293 11.707a1 1 0 0 1 1 1v7h-4v-7a1 1 0 0 1 .883 -.993l.117 -.007z"
+				/></svg
+			>
+		</a>
+		<div class="pr-2 w-fit text-center flex items-center space-x-2">
+			<img class="w-12 h-12" src="/clg-logo.png" alt="" />
+			<div class="text-2xl">St. Francis College</div>
+		</div>
+	</div>
+</div>
--- src/routes/sports/[slug]/+page.svelte
+++ src/routes/sports/[slug]/+page.svelte
@@ -2,11 +2,81 @@
 	import { page } from '$app/stores';
 	import medalImage from '../../../assets/medal.png';
 
-	let winner = '';
-
-	let teamA = 'St. Francis College';
-
-	let teamB = 'ABC College';
+	let rounds = [
+		{
+			roundNumber: 1,
+			status: 'ended',
+			endTime: '12:30PM',
+			matches: [
+				{
+					matchNumber: 1,
+					teamA: 'St. Francis College',
+					teamB: 'ABC College',
+					status: 'ended',
+					teamAScore: 20,
+					teamBScore: 19
+				},
+				{
+					matchNumber: 2,
+					teamA: 'Christ College',
+					teamB: 'BNU College',
+					status: 'ended',
+					teamAScore: 14,
+					teamBScore: 17
+				},
+				{
+					matchNumber: 3,
+					teamA: 'JNC College',
+					teamB: 'BNU College',
+					status: 'ended',
+					teamAScore: 9,
+					teamBScore: 18
+				},
+				{
+					matchNumber: 4,
+					teamA: 'NYC College',
+					teamB: 'BNU College',
+					status: 'ended',
+					teamAScore: 19,
+					teamBScore: 12
+				}
+			]
+		},
+		{
+			roundNumber: 2,
+			status: 'ongoing',
+			matches: [
+				{
+					matchNumber: 1,
+					teamA: 'St. Francis College',
+					teamB: 'ABC College',
+					status: 'ended',
+					teamAScore: 20,
+					teamBScore: 19
+				},
+				{
+					matchNumber: 2,
+					teamA: 'Christ College',
+					teamB: 'BNU College',
+					status: 'ended',
+					teamAScore: 14,
+					teamBScore: 17
+				},
+				{
+					matchNumber: 3,
+					teamA: 'JNC College',
+					teamB: 'BNU College',
+					status: 'ongoing'
+				},
+				{
+					matchNumber: 4,
+					teamA: 'NYC College',
+					teamB: 'BNU College',
+					status: 'ongoing'
+				}
+			]
+		}
+	];
 
 	function reverseSlug(slug: string) {
 		return slug
@@ -21,41 +91,84 @@
 		<h1 class="text-4xl">{reverseSlug($page.params.slug)}</h1>
 	</section>
 	<section>
-		<div class="bg-white p-2 rounded-2xl divide-y divide-black/10 text-black">
-			<div class="pb-1 flex justify-between">
-				<div class="text-lg font-semibold px-2">
-					<span class="text-green-500">Ongoing Match</span> <span>-</span>
-					<span class="text-amber-500">Round 3</span>
-				</div>
-			</div>
-			<div class="flex flex-col justify-between items-center py-2">
-				<div class="w-full text-sm">
-					{#if winner === ''}
-						<div class="w-full font-semibold text-center mx-auto grid grid-cols-5 justify-evenly">
-							<div class="grid grid-cols-3 col-span-2">
-								<div class="flex justify-center items-center col-span-2">{teamA}</div>
-								<div class="flex justify-center items-center col-span-1">(20)</div>
-							</div>
-							<div class="text-amber-500 flex justify-center items-center col-span-1">VS</div>
-							<div class="grid grid-cols-3 col-span-2">
-								<div class="flex justify-center items-center col-span-1">(18)</div>
-								<div class="flex justify-center items-center col-span-2">{teamB}</div>
-							</div>
+		{#each rounds as round}
+			{#if round.status === 'ongoing'}
+				<div class="bg-white p-2 rounded-2xl divide-y divide-black/10 text-black">
+					<div class="pb-1 flex justify-between">
+						<div class="text-lg font-semibold px-2">
+							<span class="text-green-500">Ongoing</span> <span>-</span>
+							<span class="text-amber-500">Round {round.roundNumber}</span>
 						</div>
-					{:else}
-						<div
-							class="p-0.5 rounded-full bg-white/20 w-fit font-bold text-center flex space-x-2 justify-center items-center mx-auto pr-3"
-						>
-							<img class="h-8 w-8 p-1" src={medalImage} alt="" />
-							<span class="text-amber-400">{winner}</span>
+					</div>
+					<div class="flex flex-col justify-between items-center">
+						<div class="w-full text-sm divide-y">
+							{#each round.matches as match}
+								{#if match.status === 'ongoing'}
+									<div class="flex items-center space-x-2">
+										<div class="px-2 font-bold text-blue-500">M{match.matchNumber}</div>
+										<div
+											class="w-full font-medium text-center mx-auto grid grid-cols-5 justify-evenly py-2"
+										>
+											<div class="flex justify-center items-center col-span-2">{match.teamA}</div>
+											<div class="text-amber-500 flex justify-center items-center">VS</div>
+											<div class="flex justify-center items-center col-span-2">{match.teamB}</div>
+										</div>
+									</div>
+								{/if}
+							{/each}
 						</div>
-					{/if}
+					</div>
+					<div class="px-2 pt-2 text-sm">Match Duration - 1:00PM - 1:40PM</div>
 				</div>
-			</div>
-			<div class="px-2 pt-2 text-sm">Match Duration - 1:00PM - 1:40PM</div>
-		</div>
+			{/if}
+		{/each}
 	</section>
 	<section>
+		<h2 class="text-2xl">Rounds Ended</h2>
+	</section>
+	<section class="space-y-4">
+		{#each rounds as round}
+			{#if round.status === 'ended'}
+				<div class="bg-white/20 rounded-2xl divide-y divide-white/20 p-2">
+					<div class="text-lg font-semibold px-2 pb-1 flex justify-between">
+						<div class="text-amber-400">Round {round.roundNumber}</div>
+						<div class="text-white">{round.endTime}</div>
+					</div>
+					{#each round.matches as match}
+						{#if match.status === 'ended'}
+							<div class="py-2 text-sm">
+								<div class="px-2 font-bold text-yellow-500">Match {match.matchNumber}</div>
+								<div
+									class="w-full font-medium text-center mx-auto grid grid-cols-5 justify-evenly p-2"
+								>
+									<div class="col-span-2">
+										<div class="flex flex-col justify-center items-center h-full">
+											{#if match.teamAScore > match.teamBScore}
+												<img class="w-8 h-8" src={medalImage} alt="" />
+											{/if}
+											<div>{match.teamA}</div>
+											<div>Score: {match.teamAScore}</div>
+										</div>
+									</div>
+									<div class="text-amber-400 flex justify-center items-center col-span-1">VS</div>
+									<div class="col-span-2">
+										<div class="flex flex-col justify-center items-center h-full">
+											{#if match.teamAScore < match.teamBScore}
+												<img class="w-8 h-8" src={medalImage} alt="" />
+											{/if}
+											<div>{match.teamB}</div>
+											<div>Score: {match.teamBScore}</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						{/if}
+					{/each}
+				</div>
+			{/if}
+		{/each}
+	</section>
+	<!-- <section>
 		<h2 class="text-2xl">Upcoming</h2>
 	</section>
 	<section class="space-y-4">
@@ -89,5 +202,5 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</section> -->
 </main>
