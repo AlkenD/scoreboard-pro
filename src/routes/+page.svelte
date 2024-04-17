237fee9dac115593bfb17e1c0c7811b3d87e21b1
AlkenD <96910178+AlkenD@users.noreply.github.com>
2024-04-17 10:35:19 +0000

    feat: icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,30 +41,27 @@
 			<thead class="text-orange-500 uppercase bg-gray-50">
 				<tr>
 					<th scope="col" class="px-6 py-3"> Class </th>
-					<th scope="col" class="px-6 py-3 text-center"> Scores </th>
+					<th scope="col" class="px-2 py-3 text-center"> Scores </th>
 				</tr>
 			</thead>
 			{#if loaded}
-				<!-- <tbody>
-				{#each allSports.slice(0, 5) as sport}
-					<tr class="bg-white border-b">
-						<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-							{sport}
-						</th>
-						<td class="px-6 py-4 text-blue-500 text-center"> <a href="/">View</a> </td>
-					</tr>
-				{/each}
-			</tbody> -->
 				<tbody>
 					{#await sheetsToJson('AIzaSyC5XzS785yPWZ79swN0mpPW3XVy7Duf5SY', '1sm_rPFo7xDSPuWHn4MG8xMVe8DKZL1KlgSYFhqvDDDk', 'SCORES')}
 						<p>...waiting</p>
 					{:then items}
-						{#each items.sort((a, b) => b.total - a.total).slice(0, 5) as item}
-							<tr class="bg-white border-b">
+						{#each items.sort((a, b) => b.total - a.total).slice(0, 10) as item, index}
+							<tr class="bg-white border-b space-x-2">
 								<th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-									{item.class}
+									<div class="flex items-center">
+										{#if index === 0}
+											<img class="h-8 w-8 aspect-square mr-4" src="https://cdn-icons-png.flaticon.com/512/5807/5807380.png" alt="">
+										{:else}
+											<div class="mr-8 pl-2">{index+1}</div>
+										{/if} 
+										<div>{item.class}</div>
+									</div>
 								</th>
-								<td class="px-6 py-4 text-gray-900 text-center"> {item.total} </td>
+								<td class="px-6 py-2 text-gray-900 text-center"> {item.total} </td>
 							</tr>
 						{/each}
 					{:catch error}
